e7276bb744e9eab440fcf93cdea4e934191485d3
Sam Kimball <jibequan@gmail.com>
2018-04-26 15:44:43 -0500

    Finished components

--- fake-pinterest/src/Components/Profile.js
+++ fake-pinterest/src/Components/Profile.js
@@ -1,30 +1,16 @@
 import React, {Component} from 'react';
+import ProfileText from './profiletext.js';
+import ProfilePic from './profilepic.js';
 
 class Profile extends Component {
 	render() {
 		return (
-			<div className="col-10 profile_main mx-auto row">
-
-	          <div className="col-9 profile_info">
+			<div className="col-12 profile_main mx-auto row">
+				<ProfileText />
 	          
-	            <div className="col-12 username">Name</div>
-	            <div className="row profile_bios">
-	              <div className="col-4 bio">
-	                <h3>Following</h3>
-	                <p>This is a paragraph</p>
+	             <ProfilePic />
+	          </div>      
 
-	              </div>
-	              <div className="col-4 bio">
-	                <h3>Followers</h3>
-	                <p>This is a paragraph</p>
-	              </div>
-	              <div className="col-4 bio">
-	                <h3>About</h3>
-	                <p>This is a paragraph</p>
-	              </div>
-	            </div>
-                </div>
-            </div>
 			)
 	}
 }
