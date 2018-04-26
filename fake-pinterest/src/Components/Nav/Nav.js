1d01357eb4e335bbf89f06e992bb1def5592fcf6
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-04-26 11:48:55 -0500

    css styling on the nav

--- fake-pinterest/src/Components/Nav/Nav.js
+++ fake-pinterest/src/Components/Nav/Nav.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import '../../App.css';
 import squiggle from './img/squiggleline.png';
 
 
 function Nav() {
     return (
         <nav className="d-flex justify-content-between">
-            <button type="button"><img src={squiggle} alt="" /></button>
+            <button className="nav-button" type="button"><img src={squiggle} alt="" /></button>
             <input placeholder="Search" type="text" />
-            <button type="button">Home</button>
-            <button type="button">Following</button>
-            <button type="button">Explore</button>
-            <button type="button"><img src={squiggle} alt="" />Lindsay</button>
-            <button type="button"><img src={squiggle} alt="" /></button>
-            <button type="button"><img src={squiggle} alt="" /></button>       
+            <button className="nav-button" type="button">Home</button>
+            <button className="nav-button" type="button">Following</button>
+            <button className="nav-button" type="button">Explore</button>
+            <button className="nav-button" type="button"><img src={squiggle} alt="" />Lindsay</button>
+            <button className="nav-button" type="button"><img src={squiggle} alt="" /></button>
+            <button className="nav-button" type="button"><img src={squiggle} alt="" /></button>       
         </nav>
     );
 }
